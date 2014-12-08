c669668292be98bf3aaa9bf1b10d7403edd0783d
Klas Lindfors <klas@yubico.com>
2014-12-08 12:25:40 +0100

    add upload target for mac

--- macosx.mk
+++ macosx.mk
@@ -57,3 +57,13 @@ doit:
 	cd .. && \
 	cd root && \
 	zip -r ../../$(PACKAGE)-$(VERSION)-mac.zip *
+
+upload:
+	@if test ! -d "$(YUBICO_WWW_REPO)"; then \
+		echo "www repo not found!"; \
+		echo "Make sure that YUBICO_WWW_REPO is set"; \
+		exit 1; \
+		fi
+	gpg --detach-sign --default-key $(PGPKEYID) $(PACKAGE)-$(VERSION)-mac.zip
+	gpg --verify $(PACKAGE)-$(VERSION)-mac.zip.sig
+	$(YUBICO_WWW_REPO)/publish $(PACKAGE) $(VERSION) $(PACKAGE)-$(VERSION)-mac.zip*
