3734a39247d57a3db08cb6dccc7a70b21f26b86b
Simon Josefsson <simon@josefsson.org>
2015-01-08 12:47:08 +0100

    Update license and copyright.

--- src/u2f-host.c
+++ src/u2f-host.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2013-2014 Yubico AB
+  Copyright (C) 2013-2015 Yubico AB
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
