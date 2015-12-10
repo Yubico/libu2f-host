af6e39330038e292d1de502b84720bce3f5a9ac7
dhenot <david.denot@dashlane.com>
2015-12-10 12:20:54 +0100

    Put declarations before statements for C89 compatibility

--- u2f-host/authenticate.c
+++ u2f-host/authenticate.c
@@ -271,8 +271,9 @@ u2fh_authenticate (u2fh_devs * devs,
 		   const char *challenge,
 		   const char *origin, char **response, u2fh_cmdflags flags)
 {
-  *response = NULL;
   size_t response_len = 0;
+
+  *response = NULL;
   return _u2fh_authenticate (devs, challenge, origin, response, &response_len,
 			     flags);
 }
